1f14e53d43a0151df9f2cfd54da936fd23d16b2e
Nico Lab Dev <dev@nicolab.cl>
2026-03-05 15:54:12 -0300

    feat: Add Google Analytics tracking code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,6 +43,20 @@ export default function RootLayout({
         suppressHydrationWarning
       >
         <UtmTracker />
+
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-XRX761CCKM`}
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XRX761CCKM');
+          `}
+        </Script>
         {/* Meta Pixel Code */}
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
